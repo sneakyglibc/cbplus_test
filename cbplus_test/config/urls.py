6b69b00354d9825311e3733bd2291f340bffa878
Alexandre Ghiazza <alexandre.ghiazza@oncodesign.com>
2020-09-01 13:34:57 +0200

    change the name of the api doc

--- cbplus_test/config/urls.py
+++ cbplus_test/config/urls.py
@@ -7,5 +7,6 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('', include('stock_reading.urls')),
     path('admin/', admin.site.urls),
-    path('docs/', include_docs_urls(title='API Docs', authentication_classes=[], permission_classes=[], public=True)),
+    path('api-docs/',
+         include_docs_urls(title='API Docs', authentication_classes=[], permission_classes=[], public=True)),
 ]
