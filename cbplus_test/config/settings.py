5fd65b70ef7f6ff28009d9b2db171c81e1e0c6cf
Alexandre Ghiazza <alexandre.ghiazza@oncodesign.com>
2020-09-01 16:07:01 +0200

    hedoku production + html fix + settings local and production db/url

--- cbplus_test/config/settings.py
+++ cbplus_test/config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'l!khk(tj#^)=xels#t^0)*lg*cpctn3dicgpc(end+@8up^g$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'cbplustest.herokuapp.com', '127.0.0.1']
 
 
 # Apps
@@ -58,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -84,24 +87,30 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'cbplus_test',
+            'HOST': '127.0.0.1',
+            'PASSWORD': 'cbplus_test',
+            'USER_PASSWORD': 'cbplus_test',
+            'PORT': 5432,
+        }
     }
-}"""
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'df5m2lbfqsttkr',
+            'USER': 'pbomlklsvubzll',
+            'HOST': 'ec2-52-7-15-198.compute-1.amazonaws.com',
+            'PASSWORD': '3bfc994ef06a6ecf73c2b0014f762c3c727de259706c0bdb12e70da966b83e48',
+            'PORT': 5432,
+        }
+    }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'cbplus_test',
-        'HOST': '127.0.0.1',
-        'PASSWORD': 'cbplus_test',
-        'USER_PASSWORD': 'cbplus_test',
-        'PORT': 5432,
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -140,6 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Rest Framework
 
 REST_FRAMEWORK = {
   'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
--- cbplus_test/stock_reading/views.py
+++ cbplus_test/stock_reading/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.shortcuts import render
 
 
 def stock_reading(request):
-    return render(request, 'stock/stock_reading.html', {})
+    return render(request, 'stock/stock_reading.html',
+                  {'url': 'http://127.0.0.1:8000' if settings.DEBUG else 'https://cbplustest.herokuapp.com'})
